c5a85775e516b3987196d677b557126bc5cf790a
Santiago Caro Duque <santiagocaroduque@gmail.com>
2019-08-05 20:16:42 -0500

    Update 13.py

--- Talleres/Taller 1/13.py
+++ Talleres/Taller 1/13.py
@@ -2,25 +2,43 @@
 # a --> Numero de la raiz
 # E --> Tolerancia
 # x --> Valor inicial
+import matplotlib.pyplot as plt
+import numpy as np
+
+nx = []
+ny = []
 
 
-def nth_root(a,n, x, E):
-    con=0
-    r = (1 / n) * ((n - 1) * x + (a / potencia(x, n - 1)))
-    while abs(r-x) > E:
-        con=con+1
-        x=r
-        r = (1 / n) * ((n - 1) * x + (a / potencia(x, n - 1)))
-    return r,con
+def nth_root(a, n, x, E):
+    con = 0
+    r = (1 / n) * ((n - 1) * x + (a / power(x, n - 1)))
+    while abs(r - x) > E:
+        if con > 0:
+            nx.append(r)
+        con = con + 1
+        x = r
+        r = (1 / n) * ((n - 1) * x + (a / power(x, n - 1)))
+        if con > 1:
+            ny.append(r)
+    return r, con
 
 
-
-def potencia(num, pot):
+def power(num, pot):
     r = num
     for i in range(pot - 1):
         r = r * num
     return r
 
 
-print(potencia(2, 3))
-print(nth_root(25,2,0.0001,0.1))
+print(power(2, 3))
+print(nth_root(250, 7, 0.00001, 0.000000010)) #Raiz septima de 250, con un valor inicial de 1e-5 y error de 10e-8
+
+nz = np.polyfit(nx, ny, 2)
+nf = np.poly1d(nz)
+xn_new = np.linspace(nx[0], nx[-1], 50)
+yn_new = nf(xn_new)
+
+plt.plot(nx, ny, "o", xn_new, yn_new)
+plt.xlim(min(nx) - 1, max(nx) + 1)
+plt.title(nf)
+plt.show()
