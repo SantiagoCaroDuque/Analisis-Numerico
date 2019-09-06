9f1d1da334483398f29509a7001133c4b3b7563c
Santiago Caro Duque <santiagocaroduque@gmail.com>
2019-09-06 11:43:56 -0500

    Update Punto2.py

--- Talleres/TallerInterpolacion/Punto2.py
+++ Talleres/TallerInterpolacion/Punto2.py
@@ -2,13 +2,14 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = [0, 1, 2]
-y = [10, 15, 5]
-f = interpolate.CubicSpline(x, y, bc_type=((1, 1), (1, 1)))
-xs = np.arange(-0.5, 2.5, 0.1)
-print(str(f))
+def punto2():
+  x = [0, 1, 2]
+  y = [10, 15, 5]
+  f = interpolate.CubicSpline(x, y, bc_type=((1, 1), (1, 1)))
+  xs = np.arange(-0.5, 2.5, 0.1)
+  print(str(f))
 
-plt.plot(x, y,'o',xs, f(xs), '--')
-plt.legend(["Datos","Interpolacion"])
+  plt.plot(x, y,'o',xs, f(xs), '--')
+  plt.legend(["Datos","Interpolacion"])
 
-plt.show()
+  plt.show()
